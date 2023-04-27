459820756911de5e0906e12bb80c88898aa458cc
EmilKhamatnurov <emil.khamatnurov@gmail.com>
2023-04-28 02:40:45 +0500

    Изменено поведение меню

--- scripts/burger.js
+++ scripts/burger.js
@@ -2,12 +2,14 @@
 const BURGER_OPENED_CLASSNAME = 'burger_open';
 const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
 const BODY_FIXED_CLASSNAME_2 = 'body_fixed';
+const BURGER_BUTTON_CLASS_ANIMATION = 'button_animation';
 
 const bodyNode2 = document.querySelector('body');
 const burgerNode = document.querySelector('.js-burger');
 const burgerBtnNode = document.querySelector('.js-burger-btn');
 const burgerContentNode = document.querySelector('.js-burger__content')
 
+const burgerLinesNode = document.querySelector('.burger-btn-lines');
 // Данные для анимации бургера
 // const BURGER_BTN_FIRST_LINE_ANIMATE_CLASS = 'burger-btn__first-line_animate';
 // const BURGER_BTN_SECOND_LINE_ANIMATE_CLASS = 'burger-btn__second-line_animate';	
@@ -31,7 +33,7 @@ function toggleBurger() {
     burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
     bodyNode2.classList.toggle(BODY_FIXED_CLASSNAME_2);
     burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
-	
+	burgerLinesNode.classList.toggle(BURGER_BUTTON_CLASS_ANIMATION)
 	// Анимация бургера
 	// burgerBtnFirstLine.classList.toggle(BURGER_BTN_FIRST_LINE_ANIMATE_CLASS);
 	// burgerBtnSecondLine.classList.toggle(BURGER_BTN_SECOND_LINE_ANIMATE_CLASS);
